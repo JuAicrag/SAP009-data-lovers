8644f6710ecf64e71a3edd9f1c85d8beb361bf46
julzita <juliaellen1996@gmail.com>
2023-03-30 10:59:40 -0300

    separando os repositorios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from "./data/ghibli/ghibli.js"
+
+const ghibliInfo = data;
+console.log (ghibliInfo.films[0].description);
+
+
 
 console.log(example, data);
+
+const btn = document.querySelector("#back-to-top");
+
+btn.addEventListener("click", function(){
+    window.scrollTo(0, 0);
+});
